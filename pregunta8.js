0f1045f04c6a525ce734dbe1219d1f5866f6a150
NoemiCano <noemicanoconesa@gmail.com>
2026-09-15 10:56:10 +0200

    add exercise 08

---
+++ pregunta8.js
@@ -0,0 +1,10 @@
+const URL_METEO = "https://api.open-meteo.com/v1/forecast?latitude=13.754&longitude=100.5014&daily=uv_index_max,uv_index_clear_sky_max&forecast_days=1";
+
+async function humiditySidney(url){
+    const answer = await fetch(url);
+    const data = await answer.json();
+    return data;
+}
+
+const data = await humiditySidney(URL_METEO);
+console.log("El índice UV máximo de Bangkok: ", data.daily.uv_index_max, " En cielo despejado sería: ", data.daily.uv_index_clear_sky_max);
